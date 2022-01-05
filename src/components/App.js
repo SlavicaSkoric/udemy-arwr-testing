e3044313745f4e8188bd39c9589d1856d85bb9b8
Slavica Skorić <skoric.slavica.hr@gmail.com>
2022-01-05 14:43:55 +0100

    setup app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+
+export default function App() {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+}
---
+++ src/components/CommentBox.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CommentBox() {
+  return <div>Comment Box</div>;
+}
