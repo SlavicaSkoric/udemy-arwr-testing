b6eccae5b44383aa944a1a5c164f2a6013126bdc
Slavica Skorić <skoric.slavica.hr@gmail.com>
2022-01-05 16:45:17 +0100

    add new first two tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,3 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+it('renders to screen', () => {
+  const div = document.createElement('div');
+  //fake div created w JSDOM library, solely inside the memory, inside the terminal, not created in the DOM in the browser
+
+  ReactDOM.render(<App />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('shows a comment box', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<App />, div);
+
+  //write some code that looks inside the div and checks to see if the CommentBox is in there
+  //console.log(div.innerHTML);
+  expect(div.innerHTML).toContain('Comment Box');
+
+  //cleanup after our test run
+  //optimize app performance
+  ReactDOM.unmountComponentAtNode(div);
+});
