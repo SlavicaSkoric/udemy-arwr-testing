42254041df1465a6fcf0b53cb4f4b65c62d48191
Slavica Skorić <skoric.slavica.hr@gmail.com>
2022-01-05 13:11:51 +0100

    add first two tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain('Hi there!');
 });
